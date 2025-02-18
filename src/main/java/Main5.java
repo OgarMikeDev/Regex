a99d51885e3d07e55b7301450912cc937fc8a5a6
ogar_m <ogar.m.v@mail.ru>
2025-02-18 10:58:34 +0300

    regex

---
+++ src/main/java/Main5.java
@@ -0,0 +1,24 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main5 {
+    public static void main(String[] args) {
+        String text = "Алексей, добрый день!\n" +
+                      "Мой гитхаб - https://github.com/OgarMikeDev," +
+                      "а также ссылка на мой персональный сайт - https://tommilabs.ru/\n" +
+                      "Если возникнут вопросы, пишите мне напрямую.\n" +
+                      "Я всегда доступен";
+
+        String regexForLink = "https://[^(,\\s)]+";
+
+        Pattern pattern = Pattern.compile(regexForLink);
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()) {
+            int start = matcher.start();
+            int end = matcher.end();
+            String link = text.substring(start, end);
+            System.out.println(link);
+        }
+    }
+}
